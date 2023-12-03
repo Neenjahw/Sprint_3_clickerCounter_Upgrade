0dba18536fa27b46cf86506e59c01a3d6ca5f112
Neenjahw <Nazzeroost@yandex.ru>
2023-12-03 19:39:24 +0300

    Добавлен закрытый модификатор доступа для переменных и функций, убраны лишние комментарии, переименованы переменные и функции UI-элементов, изменен порядок свойств и функций.

--- Sprint_3_clickerCounter_Upgrade/ViewController.swift
+++ Sprint_3_clickerCounter_Upgrade/ViewController.swift
@@ -9,56 +9,55 @@ import UIKit
 
 class ViewController: UIViewController, UITextViewDelegate {
     
-    @IBOutlet weak var textClicker: UILabel!
-    @IBOutlet weak var plusClicker: UIButton!
-    @IBOutlet weak var minusClicker: UIButton!
-    @IBOutlet weak var buttonReset: UIButton!
-    @IBOutlet weak var historyOfClicker: UITextView!
-    var counter: Int = 0
+    private var counter: Int = 0
+    @IBOutlet private weak var textLabel: UILabel!
+    @IBOutlet private weak var increaseButton: UIButton!
+    @IBOutlet private weak var decreaseButton: UIButton!
+    @IBOutlet private weak var resetButton: UIButton!
+    @IBOutlet private weak var historyOfClicker: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textLabel.text = "0"
+        historyOfClicker.delegate = self
+        historyOfClicker.text = "История изменений: \n"
+    }
     
     func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
         return false
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        textClicker.text = "0"
-        historyOfClicker.delegate = self
-        historyOfClicker.text = "История изменений: \n"
-    }
-    
-    func appendStringToHistoryAndSorting(_ changeDescription: String) {
+    private func appendStringToHistoryAndSorting(_ changeDescription: String) {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "[HH:mm:ss]"
         let currentDateTime = Date()
         let dateString = dateFormatter.string(from: currentDateTime)
         historyOfClicker.text.append(dateString + changeDescription + "\n")
-        
+
         let lines = historyOfClicker.text.components(separatedBy: "\n")
         let sortedLines = lines.sorted().reversed()
         let sortedText = sortedLines.joined(separator: "\n")
         historyOfClicker.text = sortedText
     }
     
-    @IBAction func buttonPlusClick(_ sender: Any) {
+    @IBAction private func increaseButton(_ sender: Any) {
         counter += 1
-        textClicker.text = String(counter)
+        textLabel.text = String(counter)
         appendStringToHistoryAndSorting("- значение увеличено на +1")
     }
     
-    @IBAction func buttonMinusClick(_ sender: Any) {
+    @IBAction private func decreaseButton(_ sender: Any) {
         if counter > 0 {
             counter -= 1
-            textClicker.text = String(counter)
+            textLabel.text = String(counter)
             appendStringToHistoryAndSorting("- значение уменьшено на -1")
         } else {
             appendStringToHistoryAndSorting("- попытка уменьшить значение ниже 0")
         }
     }
-        @IBAction func buttonDoReset(_ sender: Any) {
-            counter = 0
-            textClicker.text = String(counter)
-            appendStringToHistoryAndSorting("- значение сброшено")
+    @IBAction private func resetButton(_ sender: Any) {
+        counter = 0
+        textLabel.text = String(counter)
+        appendStringToHistoryAndSorting("- значение сброшено")
     }
 }
